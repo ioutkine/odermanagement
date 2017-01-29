a1ae141ed0b3024f5ad1cbd0695c4b27056fff94
ioutkine <ioutkine@gmail.com>
2017-01-29 19:05:51 +0800

    created Application class

---
+++ src/main/java/com/photobook/Application.java
@@ -0,0 +1,10 @@
+package com.photobook;
+
+/**
+ * Created by duck on 29/1/17.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
